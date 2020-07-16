c057238b36f0c6c18cd9cf9a4d9f334299b20cb1
Your Name <you@example.com>
2020-07-16 17:50:25 -0400

    added Pac

---
+++ routing/assets/Game/precache-manifest.bf074e7258a37c81dcab590cda92dcfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7fdf1ee92e1c6c55d30ca10ebc1aaf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f9f96c55970fe99c177f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9195b528228a3a12502a",
+    "url": "./static/js/2.d575295d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d575295d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f96c55970fe99c177f",
+    "url": "./static/js/main.b19c2200.chunk.js"
+  },
+  {
+    "revision": "fabd55e9208bf8f5534f",
+    "url": "./static/js/runtime-main.785f45fe.js"
+  }
+]);
