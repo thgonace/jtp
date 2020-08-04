ecd4da97a1eb28b5a3af554fca19ded332935ca9
Your Name <you@example.com>
2020-08-04 17:28:33 -0400

    added stock project

---
+++ routing/assets/Stocks/precache-manifest.c4959158f65455516341a464516ca00b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2804b23d16aff51dccca783f1485dab7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c47f83f4e426b75e3329",
+    "url": "./static/css/2.020302ff.chunk.css"
+  },
+  {
+    "revision": "6fd85c2a545e8ae68cbb",
+    "url": "./static/css/main.d8a8bbd1.chunk.css"
+  },
+  {
+    "revision": "c47f83f4e426b75e3329",
+    "url": "./static/js/2.7beca39e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7beca39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd85c2a545e8ae68cbb",
+    "url": "./static/js/main.ccea6150.chunk.js"
+  },
+  {
+    "revision": "baf4ca0abb282e028f9a",
+    "url": "./static/js/runtime-main.3efc0510.js"
+  },
+  {
+    "revision": "45fe765d976e9630c0ebd0ec87f38189",
+    "url": "./static/media/db.45fe765d.txt"
+  }
+]);
