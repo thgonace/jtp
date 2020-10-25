c12fbc20e5a43ffad1396afa701a1d2f234690e0
Your Name <you@example.com>
2020-10-25 17:45:30 -0400

    added poeTrade

---
+++ routing/assets/POETrade/precache-manifest.80bc03e99db799bd4f42a3f9e4680527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7a2d63795e8aa0ec360d0440b4456e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "11b9b5b354bf6be96510",
+    "url": "./static/css/main.50195734.chunk.css"
+  },
+  {
+    "revision": "a1a7a4db60b380faf942",
+    "url": "./static/js/2.4f0dfcf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4f0dfcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b9b5b354bf6be96510",
+    "url": "./static/js/main.8e3a50b6.chunk.js"
+  },
+  {
+    "revision": "7b51d61deee2eb559206",
+    "url": "./static/js/runtime-main.9fe4acb9.js"
+  }
+]);
